8a2eb1a2b78246872860293f5e3ea562ce9f0350
Mari Adamkhan <mariadamkhan@yahoo.ca>
2021-03-21 22:14:34 -0400

    all styling complete

--- sprint-two/scripts/index-page.js
+++ sprint-two/scripts/index-page.js
@@ -2,19 +2,19 @@
 let allComments = [
   {
     name: "Michael Lyons",
-    date: "12 / 18 / 2018",
+    date: "12/18/2018",
     comment:
       "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
   },
   {
     name: "Gary Wong",
-    date: "12 / 12 / 2018",
+    date: "12/12/2018",
     comment:
       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
   },
   {
     name: "Theodore Duncan",
-    date: "11 / 15 / 2018",
+    date: "11/15/2018",
     comment:
       "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
   },
@@ -66,7 +66,7 @@ function displayComments(entry) {
    commentDate.innerText = entry.date;
    commentWrap.appendChild(commentBody);
    commentBody.appendChild(commentDate);
-   let commentP = document.createElement('span');
+   let commentP = document.createElement('p');
    commentP.classList.add("default-comments__text");
    commentP.innerText = entry.comment;
    defaultComments.appendChild(commentWrap);
