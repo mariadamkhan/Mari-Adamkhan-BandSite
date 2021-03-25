8a746e72350894dca3f73f64e5b860a95bef2879
Mari Adamkhan <mariadamkhan@yahoo.ca>
2021-03-25 03:02:50 -0400

    completely rewrote JS  for shows page and updated the styling

--- sprint-three/scripts/build-shows-page.js
+++ sprint-three/scripts/build-shows-page.js
@@ -1,3 +1,84 @@
+let allShows = [
+
+  {
+    dateTitle: "Date",
+    date: "Mon Dec 17 2018",
+  
+    venueTitle: "Venue",
+    venue: "Ronald Lane",
+  
+    locationTitle: "Location",
+    location: "San Francisco, CA",
+
+    button: "Buy Tickets"
+  },
+
+  {
+    dateTitle: "Date",
+    date: "Tue Jul 18 2019",
+ 
+    venueTitle: "Venue",
+    venue: "Pier 3 East",
+ 
+    locationTitle: "Location",
+    location: "San Francisco, CA",
+
+    button: "Buy Tickets"
+  },
+
+  {
+    dateTitle: "Date",
+    date: "Fri Jul 22 2019",
+ 
+    venueTitle: "Venue",
+    venue: "View Loungue",
+  
+    locationTitle: "Location",
+    location: "San Francisco, CA",
+
+    button: "Buy Tickets"
+  },
+
+  {
+    dateTitle: "Date",
+    date: "Sat Aug 12 2019",
+  
+    venueTitle: "Venue",
+    venue: "Hyatt Agency",
+  
+    locationTitle: "Location",
+    location: "San Francisco, CA",
+
+    button: "Buy Tickets"
+  },
+
+  {
+    dateTitle: "Date",
+    date: "Fri Sep 5 2019",
+
+    venueTitle: "Venue",
+    venue: "Moscow Centre",
+  
+    locationTitle: "Location",
+    location: "San Francisco, CA",
+
+    button: "Buy Tickets"
+  },
+
+  {
+    dateTitle: "Date",
+    date: "Wed Aug 11 2020",
+ 
+    venueTitle: "Venue",
+    venue: "Pres Club",
+  
+    locationTitle: "Location",
+    location: "San Francisco, CA",
+
+    button: "Buy Tickets"
+  },
+];
+
 // creating <main> element and placing it its proper place
 // between heading & footer
 const body = document.body;
@@ -5,191 +86,111 @@ const footer = document.querySelector("footer");
 const main = document.createElement("main");
 body.insertBefore(main, footer);
 
-// creating a <section> element within <main>
-const section = document.createElement("section");
-section.classList.add("shows");
-main.appendChild(section);
+// function to create <h2> & <div> nested within <section>, and <ul>
+// nested within <div>
+function createElement() {
 
-// function creates a DOM element by
-// passing in the element name and class
-// & appends it to <section>
-function createElement(elementName, className, data) {
-  let el = document.createElement(elementName);
-  el.classList.add(className);
-  el.innerText = data;
-  section.appendChild(el);
+  const section = document.createElement("section");
+  section.classList.add("shows");
+
+  const h2Tag = document.createElement("h2");
+  h2Tag.classList.add("shows__title");
+  h2Tag.innerText = "Shows";
+
+  const divTag = document.createElement("div");
+  divTag.classList.add("shows__wrap");
+
+  const ulHeaderTag = document.createElement("ul");
+  ulHeaderTag.classList.add("shows__header");
+
+  const ulShowsTag = document.createElement("ul");
+  ulShowsTag.classList.add("shows__container");
+
+  main.appendChild(section);
+  section.appendChild(h2Tag);
+  section.appendChild(divTag);
+  divTag.appendChild(ulHeaderTag);
+  divTag.appendChild(ulShowsTag)
 }
-createElement("h2", "shows__title", "Shows");
 
-// creating a <div> element within <section>
-const div = document.createElement("div");
-div.classList.add("shows__wrap");
-section.appendChild(div);
+createElement();
 
-function createShowsCont(elementName, className, data) {
-  let el = document.createElement(elementName);
-  el.classList.add(className);
-  el.innerText = data;
-  div.appendChild(el);
-}
-// creating a <ul> section and insert into <div>
-createShowsCont("ul", "shows__header", null);
-
-// array for show headers
+// array of headings for the <ul> with class "shows__header"
 const showsHeadings = ["Dates", "Venue", "Location"];
 
-// function that creates list items with an existing array within the <ul>
-function createListItem(array) {
-  const showsHeader = document.querySelector(".shows__header");
-  const showItem = document.createElement("li");
-  showItem.classList.add("shows__categories");
-  showItem.innerText = array;
-  showsHeader.appendChild(showItem);
+// function to populate the <ul> with class "shows__header" with the above array
+function showsHeader(showsHeadings) {
+  const showsHeaderUl = document.querySelector(".shows__header");
+
+  showsHeadings.forEach(item => {
+      const listItemHeader = document.createElement("li");
+      listItemHeader.classList.add("shows__categories");
+      listItemHeader.innerText = item;
+      showsHeaderUl.appendChild(listItemHeader);
+
+  })
 }
 
-showsHeadings.forEach((item) => createListItem(item));
+showsHeader(showsHeadings);
 
-//shows <ul>
-createShowsCont("ul", "shows__container", null);
-createShowsCont("ul", "shows__container", null);
-createShowsCont("ul", "shows__container", null);
-createShowsCont("ul", "shows__container", null);
-createShowsCont("ul", "shows__container", null);
-createShowsCont("ul", "shows__container", null);
+function showsEvents(allShows) {
+  const showsUl = document.querySelector(".shows__container");
 
-let ul = document.querySelectorAll(".shows__container");
+  allShows.forEach(item =>{
+      // list item
+      const showsItem = document.createElement("li");
+      showsItem.classList.add("shows__item");
+      
+      // h3 element for date
+      const dateHeading = document.createElement("h3");
+      dateHeading.classList.add("shows__subtitle");
+      dateHeading.innerText = item.dateTitle;
+      
+      // p element for date info
+      const showsDate = document.createElement("p");
+      showsDate.classList.add("shows__data");
+      if (item.date == 2018 || 2019 || 2020 ||2021 ){
+          showsDate.classList.add("shows__data--emph")
+      }
+      showsDate.innerText = item.date;
+      
+      // h3 element for venue
+      const venueHeading = document.createElement("h3");
+      venueHeading.classList.add("shows__subtitle");
+      venueHeading.innerText = item.venueTitle;
+      
+      // p element for venue info
+      const showsVenue = document.createElement("p");
+      showsVenue.classList.add("shows__data");
+      showsVenue.innerText = item.venue;
+      
+      // h3 element for location
+      const locationHeading = document.createElement("h3");
+      locationHeading.classList.add("shows__subtitle");
+      locationHeading.innerText = item.locationTitle;
+      
+      // p element for location info
+      const showsLocation = document.createElement("p");
+      showsLocation.classList.add("shows__data");
+      showsLocation.innerText = item.location;
+      
+       // buttom element 
+      const showsButton = document.createElement("button");
+      showsButton.classList.add("shows__cta");
+      showsButton.innerText = item.button;
+      
+      // appending all newly created elements to their proper
+      // nested positions 
+      showsUl.appendChild(showsItem);
+      showsItem.appendChild(dateHeading);
+      showsItem.appendChild(showsDate);
+      showsItem.appendChild(venueHeading);
+      showsItem.appendChild(showsVenue);
+      showsItem.appendChild(locationHeading);
+      showsItem.appendChild(showsLocation);
+      showsItem.appendChild(showsButton);
 
-// objects in arrays
-let firstShow = [
-  {
-    h3: "Date",
-    p: "Mon Dec 17 2018"
-  },
-  {
-    h3: "Venue",
-    p: "Ronald Lane"
-  },
-  {
-    h3: "Location",
-    p: "San Francisco, CA"
-  },
-];
-
-let button = ["Buy Tickets"];
-
-let secondShow = [
-  {
-    h3: "Date",
-    p: "Tue Jul 18 2019"
-  },
-  {
-    h3: "Venue",
-    p: "Pier 3 East"
-  },
-  {
-    h3: "Location",
-    p: "San Francisco, CA"
-  },
-];
-
-let thirdShow = [
-  {
-    h3: "Date",
-    p: "Fri Jul 22 2019"
-  },
-  {
-    h3: "Venue",
-    p: "View Loungue"
-  },
-  {
-    h3: "Location",
-    p: "San Francisco, CA"
-  },
-];
-
-let fourthShow = [
-  {
-    h3: "Date",
-    p: "Sat Aug 12 2019"
-  },
-  {
-    h3: "Venue",
-    p: "Hyatt Agency"
-  },
-  {
-    h3: "Location",
-    p: "San Francisco, CA"
-  },
-];
-
-let fifthShow = [
-  {
-    h3: "Date",
-    p: "Fri Sep 5 2019"
-  },
-  {
-    h3: "Venue",
-    p: "Moscow Centre"
-  },
-  {
-    h3: "Location",
-    p: "San Francisco, CA"
-  },
-];
-
-let sixthShow = [
-  {
-    h3: "Date",
-    p: "Wed Aug 11 2020"
-  },
-  {
-    h3: "Venue",
-    p: "Pres Club"
-  },
-  {
-    h3: "Location",
-    p: "San Francisco, CA"
-  },
-];
-
-let allShows = [
-  firstShow,
-  secondShow,
-  thirdShow,
-  fourthShow,
-  fifthShow,
-  sixthShow,
-];
-
-// function creates 3 <li>s within a <ul>
-// with <h3> and <p> tags
-function createContainerItem(array, idx) {
-  const listItem = document.createElement("li");
-  listItem.classList.add("shows__item");
-  const h3Tag = document.createElement("h3");
-  h3Tag.classList.add("shows__subtitle");
-  h3Tag.innerText = array.h3;
-  const pTag = document.createElement("p");
-  pTag.classList.add("shows__data");
-  pTag.innerText = array.p;
-  ul[idx].appendChild(listItem);
-  listItem.appendChild(h3Tag);
-  listItem.appendChild(pTag);
+  })
 }
 
-// function creates a <button> within the 4th <li>
-// within the <ul>
-function createCtaListItem(array, idx) {
-  const listItem = document.createElement("li");
-  listItem.classList.add("shows__item");
-  const cta = document.createElement("button");
-  cta.classList.add("shows__cta");
-  cta.innerText = array;
-  ul[idx].appendChild(listItem);
-  listItem.appendChild(cta);
-}
-
-for (let i = 0; i < allShows.length; i++) {
-  allShows[i].forEach((item) => createContainerItem(item, i));
-  button.forEach((item) => createCtaListItem(item, i));
-}
+showsEvents(allShows);
